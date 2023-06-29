07c1e8ffe6f4530503dffa58d7b1e52b8e35a176
Baran <120281358+Baran-Kaya-PS@users.noreply.github.com>
2023-06-29 16:29:52 +0200

    last version last

--- src/main/java/com/baran/password/PasswordGeneratorController.java
+++ src/main/java/com/baran/password/PasswordGeneratorController.java
@@ -3,12 +3,18 @@ package com.baran.password;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PasswordGeneratorController {
-    @PostMapping
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @PostMapping("/generate")
     private String generate(@RequestParam int taille, Model modele){
         Password_gen generator = new Password_gen(taille);
         String password = generator.create();
